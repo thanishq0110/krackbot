089edaec4e04d7b441676c688ac3eb10aa5ad417
thanishq0110 <77227883+thanishq0110@users.noreply.github.com>
2021-01-18 17:55:27 +0530

    Add files via upload

---
+++ EconomyBot-main/events/botConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+    token: "Nzk5OTI4Mzk1NzM5MDM3NzI2.YAKtXw.5FYBBK8nWEAo87ennAx9bUfdjrA",
+    prefix: "-",
+    admins: [
+        "579575202610937856"
+],
+    debug: true,
+    countChannel: "countChannelID"
+};
